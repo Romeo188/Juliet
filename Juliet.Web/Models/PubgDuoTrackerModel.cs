1072705987fd3de3d7d887d5fae7d2688bb243b4
Romeo188 <vlaghost97@gmail.com>
2018-02-24 12:46:02 +0100

    Added tracker models.

---
+++ Juliet.Web/Models/PubgDuoTrackerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Juliet.Web.Models
+{
+    public class PubgDuoTrackerModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public float KdRatio { get; set; }
+        public float Top10Ratio { get; set; }
+        public float WinLossRatio { get; set; }
+    }
+}
