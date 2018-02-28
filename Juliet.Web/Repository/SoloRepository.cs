cc98aedff4dddbb0d8814a0407fc62e75f4a9649
Romeo188 <vlaghost97@gmail.com>
2018-02-28 21:11:30 +0100

    Whatever

---
+++ Juliet.Web/Repository/SoloRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using Juliet.Web.Models;
+using MySql.Data.MySqlClient;
+
+
+namespace Juliet.Web.Repository
+{
+    public class SoloRepository
+    {
+        private static MySqlConnection GetConnection()
+        {
+            var connectionString = ConfigurationManager
+        }
+    }
+}
