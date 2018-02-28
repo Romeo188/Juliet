732285d85283d0d41fb1765e5f1dc2d44f011a8b
Romeo188 <vlaghost97@gmail.com>
2018-02-28 22:02:46 +0100

    backend

---
+++ Juliet.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Web.Mvc;
+using Juliet.Web.Repository;
+
+namespace Juliet.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult GetSoloHistoryJsonResult()
+        {
+            var soloHistory = SoloRepository.GetSoloHistory();
+            return Json(soloHistory, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Juliet.Web/Repository/SoloRepository.cs
+++ Juliet.Web/Repository/SoloRepository.cs
@@ -5,6 +5,7 @@ using System.Web;
 using Dapper;
 using Juliet.Web.Models;
 using MySql.Data.MySqlClient;
+using System.Configuration;
 
 
 namespace Juliet.Web.Repository
@@ -13,7 +14,18 @@ namespace Juliet.Web.Repository
     {
         private static MySqlConnection GetConnection()
         {
-            var connectionString = ConfigurationManager
+            var connectionString = ConfigurationManager.ConnectionStrings["pubgTrackerDB"].ToString();
+            var conn = new MySqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+        public static IEnumerable<PubgSoloTrackerModel> GetSoloHistory()
+        {
+            using (var dbConnection = GetConnection())
+            {
+                var results = dbConnection.Query<PubgSoloTrackerModel>(@"SELECT * FROM solo_history;");
+                return results;
+            }
         }
     }
 }
